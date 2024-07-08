fa2512e7b882ced82444e58100b9ce399e4ab8b3
Mr.Zhou <shuangjian.zhou.com>
2024-07-08 11:43:50 +0800

    图片更新

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,13 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_dev_prokit/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
